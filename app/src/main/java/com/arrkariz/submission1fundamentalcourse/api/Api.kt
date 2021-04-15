dc8158f4b06e55d035bf91b8440f47ee18780d33
Muhammad Rizky <muhammadrisky1401@gmail.com>
2021-04-15 20:14:49 +0700

    Submission 3 add Content Provider, SQLite database, reminder

--- app/src/main/java/com/arrkariz/submission1fundamentalcourse/api/Api.kt
+++ app/src/main/java/com/arrkariz/submission1fundamentalcourse/api/Api.kt
@@ -1,14 +1,37 @@
 package com.arrkariz.submission1fundamentalcourse.api
 
+import com.arrkariz.submission1fundamentalcourse.Userdata
+import com.arrkariz.submission1fundamentalcourse.model.DetailUserResponse
 import com.arrkariz.submission1fundamentalcourse.model.UserResponse
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token a55840eaa47d01cd43ae0906e5906e88ab24299c")
+    @Headers("Authorization: token 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")
     fun getSearchUsers(
         @Query("q") query: String
     ): retrofit2.Call<UserResponse>
+
+    @GET("users/{username}")
+    @Headers("Authorization: token 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")
+    fun getUserDetail(
+            @Path("username") username: String
+    ): Call<DetailUserResponse>
+
+    @GET("users/{username}/followers")
+    @Headers("Authorization: token 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")
+    fun getFollowers(
+            @Path("username") username: String
+    ): Call<ArrayList<Userdata>>
+
+    @GET("users/{username}/following")
+    @Headers("Authorization: token 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")
+    fun getFollowing(
+            @Path("username") username: String
+    ): Call<ArrayList<Userdata>>
+
 }
--- app/src/main/java/com/arrkariz/submission1fundamentalcourse/model/User.kt
+++ app/src/main/java/com/arrkariz/submission1fundamentalcourse/model/User.kt
@@ -1,16 +1,12 @@
 package com.arrkariz.submission1fundamentalcourse
 
 import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
+import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+
 data class Userdata(
-    val avatar_url: String?,
-    val login: String?,
-    val userName: String?,
-    val location: String?,
-    val follower: String?,
-    val following: String?,
-    val company: String?,
-    val repository: String?
-): Parcelable
+    val idDb: Int,
+    val id: Int,
+    val login: String,
+    val avatar_url: String
+)
